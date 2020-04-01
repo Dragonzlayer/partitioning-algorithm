cd0dbff55a52b0f9aaa7fcf1809d1b051f247959
Dragonzlayer <48296586+Dragonzlayer@users.noreply.github.com>
2020-04-01 19:14:34 +0300

    get_parameters WIP and added main

--- init_parameters.py
+++ init_parameters.py
@@ -6,3 +6,23 @@ From user:
 
 Then: draw processing time for each machine according to the lower and upper bounds given by the user and assign p.t for each job.
 """
+import numpy as np
+
+
+MIN_NUM_MACHINES = 2
+
+
+def get_parameters():
+    while True:
+        try:
+            machines_num = int(input("How many machines: "))  # TODO make sure valid (int, >= 2) input
+            if machines_num >= MIN_NUM_MACHINES:
+                break
+        except ValueError:
+            print("Please enter a valid number, integer >= 2.")
+    jobs_num = int(input("How many jobs: "))  # TODO make sure valid - divisible by two
+    lower_num = int(input("Lower bound: "))  # TODO make sure valid
+    upper_num = int(input("Upper bound: "))  # TODO make sure valid
+    process_time = np.random.randint(lower_num, upper_num, jobs_num)
+    return process_time, machines_num
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from init_parameters import get_parameters
+
+if __name__ == '__main__':
+    process_time, machines_num = get_parameters()

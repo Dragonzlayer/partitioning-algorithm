fa21063f5f4790a3e3c7cc4119f4cbaf94d3836f
Dragonzlayer <48296586+Dragonzlayer@users.noreply.github.com>
2020-04-01 18:31:40 +0300

    added init_parameters.py requirements

---
+++ init_parameters.py
@@ -0,0 +1,8 @@
+"""
+From user:
+1:how many machines (more than two)
+2:how many jobs (need to be divisible by two)
+3: lower and upper bound of processing time
+
+Then: draw processing time for each machine according to the lower and upper bounds given by the user and assign p.t for each job.
+"""

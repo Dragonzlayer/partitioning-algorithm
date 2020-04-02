9d6d91a0bc51b834a645a753ce829e4f8085fe31
Dragonzlayer <48296586+Dragonzlayer@users.noreply.github.com>
2020-04-02 19:22:17 +0300

    adding logic WIP

--- main.py
+++ main.py
@@ -8,15 +8,30 @@ Local search:(greedy approach)
 1: put all jobs to machine #1
 
 max_machine = the machine with maximal processing time
-jobs_to_moves = jobs from max_machine
+jobs_to_move = all jobs from max_machine
+
 while jobs_to_move not empty:
-    select a job_to_move from jobs_to_moves (search space - k=1) 
-    available_machines = all_machines minus machine with maximal processing time
-    if available_machines not empty:
-        select destination_machine from available_machines
-        new_state = curr_state + job_to_move was moved to destination_machine
+
+    select a job_to_move from jobs_to_move (search space - k=1) #TODO select how? 
+    
+    available_machines = all_machines minus max_machine
+    
+    while available_machines not empty:
+    
+        select destination_machine from available_machines #TODO select how?
+        
+        new_state = curr_state plus job_to_move was moved to destination_machine
+        
         if obj_func(new_state) < obj_func(curr_state): # TODO implement more effienect look only at changes - look at delta)
+        
             curr_state = new_state
+            max_machine = the machine with maximal processing time
+            jobs_to_moves = all jobs from max_machine
+            break         
+        else:
+            available_machines = available_machines minus destination_machine
+    
+    jobs_to_move = jobs_to_move minus job_to_move                      
             
     
 5. if obj func can be improved(ie remaining available jobs > 0):

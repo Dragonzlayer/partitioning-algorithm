2be1527cc41685731317b50fdb3de6b2643affaa
Dragonzlayer <48296586+Dragonzlayer@users.noreply.github.com>
2020-04-03 21:17:28 +0300

    local search - squared sum as obj func

--- main.py
+++ main.py
@@ -55,35 +55,35 @@ from init_parameters import get_parameters
 
 def transfer_jobs(curr_state, search_space):
     is_changed = False
-    sum_pt = [sum(element) for element in machines]
+    sum_pt = [sum(np.array(element)**2) for element in curr_state]
     sum_pt = np.array(sum_pt)
     max_sum_pt = max(sum_pt)
     max_machine = np.argmax(sum_pt)  # TODO check if working
-    print("max machine = ", max_machine)
+    # print("max machine = ", max_machine)
 
     jobs_to_move = list(curr_state[max_machine])
-    print("jobs to move = ", jobs_to_move)
+    # print("jobs to move = ", jobs_to_move)
 
     while jobs_to_move:
 
         job_to_move = jobs_to_move.pop(0)  # TODO check for improvements - right now it pops the first/last element
-        print(job_to_move)
+        # print(job_to_move)
 
         available_machines = [i for i in range(machines_num)]
 
-        print("available machines: ", available_machines)
+        # print("available machines: ", available_machines)
 
         available_machines.pop(max_machine)
 
-        print("available machines after removal of max_machine: ", available_machines)
+        # print("available machines after removal of max_machine: ", available_machines)
 
         while available_machines:
 
             destination_machine = available_machines[0]
 
             temp_sum_pt = copy.deepcopy(sum_pt)
-            temp_sum_pt[max_machine] -= job_to_move
-            temp_sum_pt[destination_machine] += job_to_move
+            temp_sum_pt[max_machine] -= job_to_move**2
+            temp_sum_pt[destination_machine] += job_to_move**2
 
             max_temp_sum_pt = max(temp_sum_pt)
 
@@ -107,7 +107,7 @@ def transfer_jobs(curr_state, search_space):
             else:
                 available_machines.remove(destination_machine)
 
-            print("current state: ", curr_state)
+            # print("current state: ", curr_state)
 
     return is_changed
 
@@ -115,20 +115,20 @@ def transfer_jobs(curr_state, search_space):
 if __name__ == '__main__':
     process_time, machines_num = get_parameters()
 
-    machines = [[] for machine in range(machines_num)]
-    print(machines)
-    machines[0] = process_time
-    print(machines)
+    curr_state = [[] for machine in range(machines_num)]
+    # print(machines)
+    curr_state[0] = process_time
+    # print(machines)
 
     min_search_space = 1
     search_space = min_search_space
-    max_search_space = len(machines[0]) - 1
+    max_search_space = len(curr_state[0]) - 1
 
-    curr_state = machines
-
+    print(curr_state)
     while search_space < max_search_space:
 
         is_changed = transfer_jobs(curr_state, search_space)
+        print(curr_state)
         if not is_changed:
             break
         else:

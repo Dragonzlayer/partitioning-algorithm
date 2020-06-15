32645e1720c47c26dd58ad50088b7a72c8152cde
Dragonzlayer <48296586+Dragonzlayer@users.noreply.github.com>
2020-06-16 01:09:41 +0300

    lots of changes Bugs fixes, converges to maximum :(

--- Genetic.py
+++ Genetic.py
@@ -6,6 +6,10 @@ from heapq import nsmallest
 import sys
 import random
 from itertools import combinations
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.special import softmax
+
 """"
 Pseudocode version 1:
 
@@ -22,9 +26,9 @@ Pseudocode version 1:
 """
 np.random.seed(0)
 random.seed(1)
-num_of_chromosomes = 20
-XO_parameter = 3 # parameter that stores the numbers of chromosomes we'll perform XO over
-num_generations = 3 # just initialization - change when necessary
+num_of_chromosomes = 100
+XO_parameter = 49  # parameter that stores the numbers of chromosomes we'll perform XO over
+num_generations = 1000  # just initialization - change when necessary
 
 
 class genetic:
@@ -43,12 +47,10 @@ class genetic:
 
         # the complete self.population_sample, initializing the matrix with '-1' in every entry
         self.population_sample = -1 * np.ones((self.num_of_chromosomes, self.number_of_genes), dtype=int)
-        self.sum_obj_functions = 0  # initializing - check if it's the correct type
+        self.sum_fitness_functions = 0  # initializing - check if it's the correct type
         self.mutated_indexes = []  # stores indexes of chromosomes who've been mutated
         self.sum_input_data = np.sum(input_data)
-        self.XO_partners = -1 * np.ones((XO_parameter,2 ), dtype=int)
-
-
+        self.XO_partners = -1 * np.ones((XO_parameter, 2), dtype=int)
 
     def action(self):
         """
@@ -57,41 +59,57 @@ class genetic:
             # TODO: Add documentation
 
         """
-        """
-            print(self.input_data)
-            self.objective_function_value = self.objective_func_calc(self.population_sample)
-            print("First objective function value: ", self.objective_function_value)
-            # print("decoded:", self.decoding(self.population_sample[0]))
-            print((self.calc_probabilities(self.population_sample)))
-            print(self.sum_obj_functions)
-            self.fitness_func_calc()
-            print(self.objective_function_value)
-            # print(self.population_sample)
-            self.perform_Mutation()
-            # print("After mutation:")
-            # print(self.population_sample)
-            # for indx in range(self.num_of_chromosomes):
-            self.correction(np.array([1,1,1,1,1,0,0,0,0,0]))
-        """
-        print("Starting Genetic")
-        self.create_population()
 
+        # print(f"{self.input_data}")
+        # self.objective_function_value = self.objective_func_calc(self.population_sample)
+        # print("First objective function value: ", self.objective_function_value)
+        # print("decoded:", self.decoding(self.population_sample[0]))
+        # print((self.calc_probabilities(self.population_sample)))
+        # print(self.sum_fitness_functions)
+        # self.fitness_fuc_calc()
+        # print(self.objective_function_value)
+        # print(self.population_sample)
+        # self.perform_Mutation()
+        # print("After mutation:")
+        # print(self.population_sample)
+        # for indx in range(self.num_of_chromosomes):
+        #     self.correction(np.array([1,1,1,1,1,0,0,0,0,0]))n
+
+        print("Starting Genetic")
+        print(f"{self.input_data}")
+        self.create_population()
+        print("Population:\n", self.population_sample)
+        res = []
         for generation in range(num_generations):
-            print(" ------------ New generation")
+            print(" ------------ New generation ------------")
             self.mutated_indexes.clear()
-            self.objective_function_value = self.objective_func_calc(self.population_sample)
-            self.calc_probabilities(self.population_sample)
+            self.objective_func_calc()
+            print(self.objective_function_value)
             self.fitness_func_calc()
+            print(self.fitness_func)
+            self.calc_probabilities()
+            print(self.probabilities)
             self.elitism()
+            self.elitism()
+            self.elitism()
+            print(self.population_sample[self.mutated_indexes])
+            print(self.decoding(self.population_sample[self.mutated_indexes][0]))
             self.perform_Mutation()
+            print(self.population_sample[self.mutated_indexes[1:]])
+            print("XO")
             self.choose_parents_for_XO()
+            print(self.XO_partners)
             for indx in range(XO_parameter):
                 # TODO: how to choose them using fitnees function?
-                self.perform_XO(self.XO_partners[indx,0], self.XO_partners[indx, 1])
+                self.perform_XO(self.XO_partners[indx, 0], self.XO_partners[indx, 1])
 
-            print(self.population_sample)
+            res.append(self.objective_function_value.mean())
 
-
+        print(self.population_sample)
+        print(self.objective_function_value)
+        sns.set()
+        plt.plot(res)
+        plt.show()
 
     def create_population(self):
         """
@@ -138,9 +156,9 @@ class genetic:
                 chromosome_i[random_index1] = random_machine
                 chromosome_i[random_index2] = random_machine
 
-        print(self.population_sample)
+        # print(self.population_sample)
 
-    def objective_func_calc(self, sample):
+    def objective_func_calc(self):
         """
         calculates objective function value by iterating every chromosome, and for each chromosome use decoding method
         to find the process time of each machine.
@@ -149,14 +167,14 @@ class genetic:
         Args:
             sample: The current population_sample
 
-        Returns: Objective function value - max process time of every chromosome
+        Updates Objective function value - max process time of every chromosome
 
         """
+        print("calculating objective function value:")
 
         for i in range(self.num_of_chromosomes):
-            self.objective_function_value[i] = np.max(self.decoding(sample[i]))
+            self.objective_function_value[i] = np.max(self.decoding(self.population_sample[i]))
 
-        return self.objective_function_value
 
     # TODO: I've added 1 so that no chromosome will receive the value '0' - check if OK or what/how to fix
     def fitness_func_calc(self):
@@ -167,9 +185,11 @@ class genetic:
         Returns: None
 
         """
-        self.fitness_func = (self.sum_input_data + 1) - self.objective_function_value
+        print("Calculating fitness function:")
 
-        print(self.fitness_func)
+        self.fitness_func = self.sum_input_data + 1 - self.objective_function_value
+
+        # print(self.fitness_func)
 
     # TODO: check if actually working, he
     def choose_parents_for_XO(self):
@@ -180,9 +200,12 @@ class genetic:
         """
         # np array that contains the pairs of chromosomes for XO
 
+        available_to_xo_indexes = list(set(range(num_of_chromosomes)) - set(self.mutated_indexes))
+        available_to_xo_indexes.sort()
         for i in range(XO_parameter):  # TODO: check if it's actually working
-            x = np.array(random.choices(list(range(num_of_chromosomes)), weights=self.probabilities, k=2))
+            x = np.array(random.choices(available_to_xo_indexes, weights=self.probabilities[available_to_xo_indexes], k=2))
             self.XO_partners[i] = x
+
     # TODO: check if working, and if giving 2 chromosomes is neccessary
     # TODO: first keep working on that!
     def perform_XO(self, index_1, index_2):
@@ -202,7 +225,7 @@ class genetic:
 
         # sending the chromosomes to self.correction() in case the XO caused for invalid partition
         self.correction(chrome1)
-        self.correction(chrome1)
+        self.correction(chrome2)
 
         # return chromosome_1, chromosome_2
 
@@ -221,38 +244,41 @@ class genetic:
 
         """
         mutation_prob = random.uniform(0.001, 0.05)
-        print(f"{mutation_prob=}")
+
+        print("Performimg Mutation with mutation probability:", mutation_prob)
+
         for i in range(self.num_of_chromosomes):
             # Don't perform mutation on elitist chromosome
             # if there's more than one elitist chromosome - perform the comparison accordingly
-            if i == self.mutated_indexes[0]:
-                pass
-        else:
-            # draw random between 0 and 1
-            x = random.random()
-            if x < mutation_prob:
-                position1 = random.randint(0, self.number_of_genes-1)
-                position2 = random.randint(0, self.number_of_genes - 1)
-                temp = position2
-                self.population_sample[i][position2] = self.population_sample[i][position1]
-                self.population_sample[i][position1] = self.population_sample[i][temp]
-                self.mutated_indexes.append(i)
-                # print(f"mutate chrome {i}, gene {position} to machine {machine}")
+            if i not in self.mutated_indexes:
+                # draw random between 0 and 1
+                x = random.random()
+                if x < mutation_prob:
+                    position1 = random.randint(0, self.number_of_genes - 1)
+                    position2 = random.randint(0, self.number_of_genes - 1)
+                    temp = self.population_sample[i][position2]
+                    self.population_sample[i][position2] = self.population_sample[i][position1]
+                    self.population_sample[i][position1] = temp
+                    self.mutated_indexes.append(i)
+                    # print(f"mutate chrome {i}, gene {position} to machine {machine}")
 
     # TODO: check how to calculate the probabilities
-    def calc_probabilities(self, population):
+    def calc_probabilities(self):
         """
         calculating probabilities for current population_sample-
         i.e - chromosome index, the chromosome itself, x_i (check again), choosing probability
         Returns: Matrix representation of current population_sample's data
 
         """
-        self.sum_obj_functions = np.sum(self.objective_function_value)
-        self.probabilities = self.objective_function_value / self.sum_obj_functions
-        for i in range(self.num_of_chromosomes):
-            print(
-                f'ID {i}: {population[i]} function value: {self.objective_function_value[i]} choosing prob:{round(self.probabilities[i], 6)}')
-            # population_data[i] =[f'chromosome index: {i} chromosome: {population_sample[i]} function value: {self.objective_function_value[i]} choosing probability:{self.objective_function_value[i] / sum_obj_functions}']
+        print("Calculating probabilities:")
+
+        # self.probabilities = self.fitness_func / np.sum(self.fitness_func)
+        self.probabilities = softmax(self.fitness_func)
+
+        # for i in range(self.num_of_chromosomes):
+        # print(
+        #    f'ID {i}: {population[i]} function value: {self.objective_function_value[i]} choosing prob:{round(self.probabilities[i], 6)}')
+        # population_data[i] =[f'chromosome index: {i} chromosome: {population_sample[i]} function value: {self.objective_function_value[i]} choosing probability:{self.objective_function_value[i] / sum_obj_functions}']
 
     def elitism(self):
         """
@@ -263,6 +289,7 @@ class genetic:
         Returns: None
 
         """
+        print("Elitism")
         index_for_elitism = np.argmax(self.fitness_func)
         self.mutated_indexes.append(index_for_elitism)
 
@@ -297,7 +324,6 @@ class genetic:
         # stores the unique machines values in ascending order
         # stores the frequency for each machine - i.e how many jobs are assigned to each machine
 
-        i = 0
         # array that stores 0 if the machine with the current index has even number of jobs - and 1 if it's odd
         # odd_even = np.zeros(self.number_of_machines)
         # uniques stores uniques machine values
@@ -309,15 +335,14 @@ class genetic:
             if count % 2 == 1:
                 odd_machines.append(machine)
 
-        pairs =[]
-        for i in range(0,len(odd_machines), 2):
-            pairs.append((odd_machines[i], odd_machines[i+1]))
+        pairs = []
+        for i in range(0, len(odd_machines), 2):
+            pairs.append((odd_machines[i], odd_machines[i + 1]))
 
         for i, j in pairs:
-            print(decoded_chromosome[i])
-            print(decoded_chromosome[j])
+            # print(decoded_chromosome[i])
+            # print(decoded_chromosome[j])
             if decoded_chromosome[j] > decoded_chromosome[i]:
-
 
                 masked = np.ma.MaskedArray(self.input_data, chromosome != j)
                 index_min_job = np.ma.argmin(masked)
@@ -328,25 +353,21 @@ class genetic:
                 index_min_job = np.ma.argmin(masked)
                 chromosome[index_min_job] = j
 
-        print(chromosome)
+        # print(chromosome)
 
-        # todo delete below
-        uniques, frequencies = np.unique(chromosome, return_counts=True)
-        odd_machines = []
-        for machine, count in zip(uniques, frequencies):
-            if count % 2 == 1:
-                odd_machines.append(machine)
-        if odd_machines:
-            print("ERRRORRRRR")
-        else:
-            print("NO ERRORRRR")
-
+        # # todo delete below
+        # uniques, frequencies = np.unique(chromosome, return_counts=True)
+        # odd_machines = []
+        # for machine, count in zip(uniques, frequencies):
+        #     if count % 2 == 1:
+        #         odd_machines.append(machine)
+        # if odd_machines:
+        #     print("ERRRORRRRR")
+        # else:
+        #     print("NO ERRORRRR")
 
         # for index in odd_even:
         # more from machine with higher sum the job with minimal val to the other machine in the pair
-
-
-
 
 # TODO: when there's more than 2 machines - check what to do when some chromosomes don't assign jobs to a certain machine
 # TODO decoding method to visually see the partition
